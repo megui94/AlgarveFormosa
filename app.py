450270104fd05822bd9843472d166317e01a18ab
Ana Pedro <anapedro0813@gmail.com>
2025-11-27 17:40:39 +0000

    Primeira versão do site Algarve Friends Tour

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", active_page="home")
+
+
+@app.route("/servicos")
+def servicos():
+    return render_template("servicos.html", active_page="servicos")
+
+
+@app.route("/tours")
+def tours():
+    return render_template("tours.html", active_page="tours")
+
+
+@app.route("/sobre")
+def about():
+    return render_template("about.html", active_page="about")
+
+
+@app.route("/contactos")
+def contactos():
+    return render_template("contactos.html", active_page="contactos")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
